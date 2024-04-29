8dcd4f6320d57d4d0e9ffb0e9fc46ccd0d071315
Avellar <99830352+YagoAvellar@users.noreply.github.com>
2024-04-29 14:40:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,29 @@ function retornaPaginaInicial(requisicao, resposta){
     resposta.end();
 };
 
-
+function enviarDinheiro(requisicao, resposta){
+    //extrair da url da requisição o valor desejado pelo usuário  
+    let valorDesejado = requisicao.query.valor;
+    if (!valorDesejado){
+        valorDesejado = 0;
+    }
+    resposta.write('<!DOCTYPE html>');
+    resposta.write('<html>');
+    resposta.write('<head>');
+    resposta.write('<meta charset="utf-8">');
+    resposta.write('<title>Oferta de Dinheiro</title>');
+    resposta.write('</head>');
+    resposta.write('<body>');
+    if (valorDesejado > 0){
+        resposta.write('<h1>Toma aqui seus R$ ' + valorDesejado + '</h1>');
+    }
+    else{
+        resposta.write('<h1>Informe o parâmetro valor na url:   http://localhost:3000/dinheiro?valor=100</h1>');
+    }
+    resposta.write('</body>');
+    resposta.write('</html>');
+    resposta.end();
+}
 
 function converterCelsiusFahrenheit(requisicao, resposta){
     //extrair da url da requisição o(s) grau(s) celcius a ser convertido
@@ -58,52 +80,12 @@ function converterCelsiusFahrenheit(requisicao, resposta){
     resposta.write('</html>');
     resposta.end();
 }
-function calcularTabuada(requisicao, resposta) {
-    // Extrair da URL da requisição o número para o qual a tabuada será calculada
-    let numero = requisicao.query.numero;
-    let sequencia = requisicao.query.sequencia;
-    
-    // Verificar se foi fornecido um parâmetro sequência e definir para 10 se não houver
-    if (!sequencia) {
-        sequencia = 10;
-    }
-    
-    // Escrever a estrutura básica do HTML na resposta
-    resposta.write('<!DOCTYPE html>');
-    resposta.write('<html>');
-    resposta.write('<head>');
-    resposta.write('<meta charset="utf-8">');
-    resposta.write('<title>Tabuada</title>');
-    resposta.write('</head>');
-    resposta.write('<body>');
-    
-    // Verificar se um número foi fornecido na URL
-    if (numero) {
-        // Converter o número fornecido para um valor inteiro
-        numero = parseInt(numero);
-        
-        // Converter a sequência fornecida para um valor inteiro
-        sequencia = parseInt(sequencia);
-        
-        // Calcular e exibir a tabuada do número fornecido
-        for (let i = 1; i <= sequencia; i++) {
-            const resultado = numero * i;
-            resposta.write('<h1>' + numero + ' x ' + i + ' = ' + resultado + '</h1>');
-        }
-    } else {
-        // Se nenhum número foi fornecido na URL, exibir uma mensagem solicitando que um número seja fornecido
-        resposta.write('<h1>Informe o parâmetro numero na URL: http://localhost:3000/tabuada?numero=</h1>');
-    }
-    
-    // Fechar a estrutura HTML na resposta
-    resposta.write('</body>');
-    resposta.write('</html>');
-    resposta.end();
-}
-
+//Javascript, além de valores dos tipos: string, decimal, inteiro,booleano
+//também permite que funções sejam passadas como parâmetros para outras funções.
 app.get("/", retornaPaginaInicial);
+app.get("/dinheiro", enviarDinheiro);
 app.get("/conversor", converterCelsiusFahrenheit);
-app.get("/tabuada", calcularTabuada);
+//( ) => {}  = função anônima conhecida como arrow function
 app.listen(porta, host, () => {
     console.log("Servidor está executando em http://" + host + ":" + porta);
 });
